b38d35a39310c9a8b4d5ce5ed07f71b80e0eb33b
Fabian Hirter <mail@fabianhirter.ch>
2023-02-20 19:50:50 +0100

    add type hints

--- Examples/Python/MVC_Observer/subject.py
+++ Examples/Python/MVC_Observer/subject.py
@@ -1,10 +1,13 @@
+from Examples.Python.MVC_Observer.observer import Observer
+
+
 class Subject:
     observers = []
 
-    def attach(self, observer):
+    def attach(self, observer: Observer):
         self.observers.append(observer)
 
-    def detach(self, observer):
+    def detach(self, observer: Observer):
         pass
 
     def notify(self):
