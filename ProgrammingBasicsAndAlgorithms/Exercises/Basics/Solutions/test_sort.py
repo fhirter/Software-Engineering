3f381c30967ef300e64ccba437c00abbd87dd06f
Fabian Hirter <mail@fabianhirter.ch>
2024-10-28 17:04:38 +0100

    add solution

---
+++ ProgrammingBasicsAndAlgorithms/Exercises/Basics/Solutions/test_sort.py
@@ -0,0 +1,8 @@
+from ProgrammingBasicsAndAlgorithms.Exercises.Basics.Solutions.sort import sort
+import unittest
+
+class TestSort(unittest.TestCase):
+    def test_sort(self):
+        a = [3, 0, 12, 8]
+        b = sort(a)
+        self.assertEqual(b, [0, 3, 8, 12])
